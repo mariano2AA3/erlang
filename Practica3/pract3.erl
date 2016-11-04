e090938334613cea415573d7894a3858516a419b
supra <marianoh@ucm.es>
2016-11-04 17:02:04 +0100

    Comments added.

--- Practica3/pract3.erl
+++ Practica3/pract3.erl
@@ -4,12 +4,13 @@
 
 
 
-% Ejer 1
+% Ejercicio 1
+
 different(F,G,L) -> lists:filter(fun(X) -> F(X)/=G(X) end, L).
 
 
 
-% Ejer 2
+% Ejercicio 2
 
 abs(X) ->
 if 
@@ -25,49 +26,49 @@ different_test() -> ?assertEqual(different(fun abs/1,fun id/1,lists:seq(-3,10)),
 
 
 
-% Ejer 3
+% Ejercicio 3
 
 differentBis(F,G,L) -> [X || X <- L, F(X) /= G(X)].
 
 
 
-% Ejer 4
+% Ejercicio 4
 
 differentBis_test() -> ?assertEqual(differentBis(fun abs/1,fun id/1,lists:seq(-3,10)), [-3,-2,-1]).
 
 
 
-% Ejer 5
+% Ejercicio 5
 
 equal(F,G,L) -> length(different(F,G,L)) =:= 0.
 
 
 
-% Ejer 6
+% Ejercicio 6
 
 equal_test() -> ?assertEqual(equal(fun abs/1, fun abs/1, lists:seq(-10,10)),true).
 
 
 
-% Ejer 7
+% Ejercicio 7
 
 morosos(L) -> [element(1,X) || X <-L , element(2,X) < 0].
 
 
 
-% Ejer 8
+% Ejercicio 8
 
 morosos_test() -> ?assertEqual(morosos([ {bertoldo, 500}, {herminia,cancelada}, {aniceto,-2000} ]),[aniceto]).
 
 
 
-% Ejer 9
+% Ejercicio 9
 
 triangulo(N) -> [ [ [X || X <- lists:seq(1, Y) ] || Y <- lists:seq(1, N)] ] .
 
 
 
-% Ejer 10
+% Ejercicio 10
 
 aplicarFunciones([],X) -> [];
 aplicarFunciones([L|Ln], X) -> lists:append([L(X)], aplicarFunciones(Ln,X)).
